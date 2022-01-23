eda6039a462f2d09eb454d7af4ff72ffab38b27d
Kyle Baley <kyle@baley.org>
2022-01-23 14:45:52 -0500

    Style timetable

--- client/src/Timetable.js
+++ client/src/Timetable.js
@@ -1,18 +1,24 @@
+import { Box, Card, Typography } from '@mui/material';
 import moment from 'moment';
 
 const Timetable  = ({timetable, offset}) => {
   return (
-    <table>
-      <tbody>
+    <Box
+      component="span"
+      sx={{ display: 'inline-block', mx: '5px' }}
+    >
       {timetable.map((entry) => (
-        <tr key={entry.id}>
-          <td>{moment(entry.from).add(offset, 'minutes').format('MMM DD HH:mm')}</td>
-          <td>{entry.subject}</td>
-          <td>{entry.teacher}</td>
-        </tr>
+        <Card
+          key={entry.id}
+          sx={{ my: '10px', padding: '5px' }}
+          style={{ textAlign: 'center' }}
+        >
+          <Typography color="text.primary" gutterBottom>{moment(entry.from).add(offset, 'minutes').format('MMM DD HH:mm')}</Typography>
+          <Typography color="text.primary" gutterBottom>{entry.subject}</Typography>
+          <Typography color="text.secondary" style={{ fontSize: 12 }} gutterBottom>{entry.teacher}</Typography>
+        </Card>
       ))}
-      </tbody>
-    </table>
+    </Box>
   )
 }
 
