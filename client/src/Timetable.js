84e60ffa00b9bbce4b983b66817fa6928e91e98b
Kyle Baley <kyle@baley.org>
2022-01-23 15:02:28 -0500

    More timetable styles

--- client/src/Timetable.js
+++ client/src/Timetable.js
@@ -1,21 +1,52 @@
 import { Box, Card, Typography } from '@mui/material';
 import moment from 'moment';
 
+const boxSx = {
+  display: 'inline-block',
+  mx: '5px',
+}
+const cardSx = {
+  my: {
+    md: 1,
+    xs: 0,
+  },
+  padding: {
+    md: 1,
+    xs: 0.5,
+  },
+  transform: {
+    md: 'scale(1.0)',
+    xs: 'scale(0.8)'
+  }
+}
+
+const dateSx = {
+  fontSize: 16,
+}
+
+const subjectSx = {
+  fontSize: 18,
+}
+
+const teacherSx = {
+  fontSize: 12,
+}
+
 const Timetable  = ({timetable, offset}) => {
   return (
     <Box
       component="span"
-      sx={{ display: 'inline-block', mx: '5px' }}
+      sx={boxSx}
     >
       {timetable.map((entry) => (
         <Card
           key={entry.id}
-          sx={{ my: '10px', padding: '5px' }}
+          sx={cardSx}
           style={{ textAlign: 'center' }}
         >
-          <Typography color="text.primary" gutterBottom>{moment(entry.from).add(offset, 'minutes').format('MMM DD HH:mm')}</Typography>
-          <Typography color="text.primary" gutterBottom>{entry.subject}</Typography>
-          <Typography color="text.secondary" style={{ fontSize: 12 }} gutterBottom>{entry.teacher}</Typography>
+          <Typography color="text.primary" sx={dateSx}>{moment(entry.from).add(offset, 'minutes').format('MMM DD HH:mm')}</Typography>
+          <Typography color="text.primary" sx={subjectSx}>{entry.subject}</Typography>
+          <Typography color="text.secondary" sx={teacherSx} gutterBottom>{entry.teacher}</Typography>
         </Card>
       ))}
     </Box>
