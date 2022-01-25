8a1248101ced440767fce5a6b16e3875d41bb6c6
Kyle Baley <kyle@baley.org>
2022-01-25 17:10:38 -0500

    Nicer timetable formatting

--- client/src/Timetable.js
+++ client/src/Timetable.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Card, CardActionArea, Typography } from '@mui/material';
+import { Box, Card, CardContent, CardActionArea, Typography } from '@mui/material';
 import moment from 'moment';
 import Header from './Header';
 import { groupBy, filter } from 'lodash';
@@ -41,16 +41,18 @@ const cardSx = (backgroundColor) => {
       xs: 'scale(0.8)'
     },
     textAlign: 'center',
-    backgroundColor: `rgba(${hexToRgb(backgroundColor)}0.1)`
+    alignItems: 'center',
+    backgroundColor: `rgba(${hexToRgb(backgroundColor)}0.1)`,
+    display: 'flex',
   })
 }
 
 const dateSx = {
-  fontSize: 16,
+  fontSize: 18,
 }
 
 const subjectSx = {
-  fontSize: 18,
+  fontSize: 16,
 }
 
 const teacherSx = {
@@ -59,6 +61,7 @@ const teacherSx = {
 
 const Timetable  = ({timetable, offset}) => {
   const [firstDay, setFirstDay] = React.useState([]);
+  const [date, setDate] = React.useState("");
 
   React.useEffect(() => {
     const getDateWithoutTime = (date) => {
@@ -69,9 +72,10 @@ const Timetable  = ({timetable, offset}) => {
     if (dateToUse.getHours() > 14) {
       dateToUse.setDate(dateToUse.getDate() + 1);
     }
-    const date = getDateWithoutTime(dateToUse);
+    const dateFormatted = getDateWithoutTime(dateToUse);
+    setDate(dateFormatted);
 
-    const filtered = filter(timetable, (entry) => getDateWithoutTime(entry.from) === date);
+    const filtered = filter(timetable, (entry) => getDateWithoutTime(entry.from) === dateFormatted);
     const grouped = groupBy(filtered, (entry) => getDateWithoutTime(entry.from));
     const keys = Object.keys(grouped);
     setFirstDay(timetable.length > 0 ? grouped[keys[0]] : []);
@@ -83,25 +87,28 @@ const Timetable  = ({timetable, offset}) => {
       sx={boxSx}
     >
       <Header text="Timetable" visible={timetable.length > 0} />
+      <Typography variant="h6" sx={{textAlign: 'center'}}>{firstDay.length > 0 ? date : ""}</Typography>
       {firstDay.map((entry) => (
         <Card
           key={entry.id}
           variant="outlined"
           sx={cardSx(entry.color)}
         >
-          <CardActionArea
-            sx={{p: 1}}
-          >
-            <Typography color="text.primary" sx={dateSx} variant="body2">
-              {moment(entry.from).add(offset, 'minutes').format('MMM DD HH:mm')}
-            </Typography>
-            <Typography color="text.primary" sx={subjectSx} variant="body2">
-              {entry.subject}
-            </Typography>
-            <Typography color="text.secondary" sx={teacherSx} gutterBottom variant="body2">
-              {entry.teacher}
-            </Typography>
-          </CardActionArea>
+          <Box sx={{display: 'flex', flexDirection: 'column'}}>
+            <CardContent sx={{ flex: '1 0 auto' }}>
+              <Typography color="text.primary" sx={dateSx} variant="body2" component="div">
+                {moment(entry.from).add(offset, 'minutes').format('HH:mm')}
+              </Typography>
+            </CardContent>
+          </Box>
+          <CardContent sx={{width: '100%', borderLeft: `1px solid ${entry.color}`}}>
+              <Typography color="text.primary" sx={subjectSx} variant="body2" component="div">
+                {entry.subject}
+              </Typography>
+              <Typography color="text.secondary" sx={teacherSx} variant="body2" component="div">
+                {entry.teacher}
+              </Typography>
+          </CardContent>
         </Card>
       ))}
     </Box>
