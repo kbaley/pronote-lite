96abd4043585c9c0471ae74b5a733f8cf3f9f62f
Kyle Baley <kyle@baley.org>
2022-02-20 16:57:27 -0500

    WIP

--- client/src/PrintableTimetable.js
+++ client/src/PrintableTimetable.js
@@ -6,10 +6,6 @@ import axios from 'axios';
 import moment from 'moment';
 import { forEach } from 'lodash';
 import LoginForm from './LoginForm';
-import {
-  getBreaks,
-  getDateWithoutTime
-} from './TimetableFns';
 
 const boxSx = {
   display: 'inline-block',
@@ -17,7 +13,6 @@ const boxSx = {
   width: '100%',
 }
 const PrintableTimetable  = () => {
-  const [dayEntries, setDayEntries] = React.useState([]);
   const [timetable, setTimetable] = React.useState({});
   const [isLoggedIn, setIsLoggedIn] = React.useState(false);
 
@@ -33,36 +28,12 @@ const PrintableTimetable  = () => {
       });
       console.log(data);
       moment.suppressDeprecationWarnings = false;
-      addBreaks(data);
       setTimetable(data);
     })
     .catch( (error) => {
       console.log(error);
     });
   }, []);
-
-  const addBreaks = (filtered) => {
-    const keys = Object.keys(filtered);
-    for (let index = 0; index < keys.length; index++) {
-      const day = filtered[keys[index]];
-      filtered[keys[index]] = addBreaksToDay(day, keys[index]);
-    }
-  };
-
-  const addBreaksToDay = (entries, date) => {
-    let startDate = new Date(date);
-    console.log(entries);
-    startDate.setHours(7);
-    startDate.setMinutes(30);
-    startDate.setSeconds(0);
-
-    const clonedEntries = clone(entries);
-    const newEntries = getBreaks(entries, startDate);
-    forEachRight(newEntries, newEntry => {
-      clonedEntries.splice(newEntry.position, 0, newEntry);
-    });
-    return clonedEntries;
-  }
 
   const login = async () => {
     getStudentData();
